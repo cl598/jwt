afb52d44bdcd388f276cf11c614dd6aadc5f45bc
psyche <cl598@njit.edu>
2021-04-27 13:03:07 -0400

    re-add previous code

---
+++ proj/screenshots/readme.md
@@ -0,0 +1,9 @@
+![delete](sc_delete_id.png)
+
+![get](sc_get.png)
+
+![getid](sc_get_id.png)
+
+![post](sc_post.png)
+
+![putid](sc_put_id.png)
---
+++ proj/server/app.js
@@ -0,0 +1,39 @@
+'use strict';
+const express = require('express');
+const open = require("open");
+const bodyParser = require('body-parser');
+var cors = require('cors')
+
+// create express app
+const app = express();
+
+app.use(cors())
+app.use(express.static('docs'));
+
+// Setup server port
+const port = process.env.PORT || 5000;
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// define a root route
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+// Require employee routes
+const citiesRoutes = require('./routes/cities.routes')
+
+// using as middleware
+app.use('/api/v1/cities', citiesRoutes)
+
+app.set('port', process.env.PORT || 8000);
+app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+
+app.listen(app.get('port'), function() {
+    console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
+    open("http://localhost:8000");
+
+});
